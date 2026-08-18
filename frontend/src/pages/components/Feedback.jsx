e8c562baa28825db9bfe8a4d2f8432006dac6ad1
fahim-shareear <fahimshareear@gmail.com>
2026-08-18 13:43:26 +0600

    adding the functionality to upload image and submitting feedback to the db

--- frontend/src/pages/components/Feedback.jsx
+++ frontend/src/pages/components/Feedback.jsx
@@ -9,6 +9,7 @@ import { useEffect, useState } from 'react';
 import useAxios from '../../axios/useAxios';
 import { toast } from 'react-toastify';
 import { useForm } from "react-hook-form"
+import axios from 'axios';
 
 
 
@@ -17,7 +18,8 @@ const Feedback = () => {
     const [feedback, setFeedback] = useState([]);
     const [loading, setLoading] = useState(true);
     const [feedForm, setFeedForm] = useState(false);
-    const {register, handleSubmit,formState: {errors}} = useForm();
+    const [submitting, setSubmitting] = useState(false);
+    const {register, reset, handleSubmit,formState: {errors}} = useForm();
 
     useEffect(() => {
         axiosinstance.get('/feedback').then(res => {
@@ -38,7 +40,42 @@ const Feedback = () => {
     //handle form submission:
     const handleFormSubmit = (data) =>{
         const profileImg = data.image[0];
-    }
+        setSubmitting(true);
+
+
+        //uploading image to imgbbfirst:
+        const formData = new FormData();
+        formData.append('image', profileImg);
+
+        const image_API_URL = `https://api.imgbb.com/1/upload?key=${import.meta.env.VITE_image_host}`;
+        axios.post(image_API_URL, formData).then(res =>{
+            const photoURL = res.data.data.url;
+
+            //feedback payload:
+            const feedbackInfo = {
+                name: data.name,
+                email: data.email,
+                feedback: data.feedback,
+                photoURL: photoURL
+            };
+
+            axiosinstance.post('/feedback', feedbackInfo).then(res =>{
+                if(res.data.insertedId){
+                    toast.success("Thank you for your kind feedback!");
+                    setFeedback(prev => [...prev, {...feedbackInfo, _id: res.data.insertedId}]);
+                    reset();
+                    setFeedForm(false);
+                }
+            }).catch(() =>{
+                toast.error("Couldn't submit your feedback!")
+            }).finally(()=>{
+                setSubmitting(false);
+            })
+        }).catch(()=>{
+            toast.error("Image upload failed! Please try again.");
+            setSubmitting(false);
+        })
+    };
 
     if (loading) return <p className='text-center italic text-[#005A00] font-bold'>Loading.....</p>
 
@@ -71,11 +108,12 @@ const Feedback = () => {
                             :
                             feedback.map((f) => (
                                 <SwiperSlide key={f._id} className="w-150! h-80! rounded-xl border-2 border-[#00E5A0] shadow-[inset_0_0_40px_rgba(0,229,160,0.15)] bg-white/4">
-                                    <div className='p-8 flex items-left justify-center flex-col gap-3'>
-                                        <img src="#" alt="#" className='rounded-full w-20 h-20 border-3 border-[#00E5A0]' />
+                                    <div className='p-8 flex items-center w-full justify-center flex-col gap-2'>
+                                        <img src={f.photoURL} alt={f.name} className='rounded-full w-20 h-20 border-3 border-[#00E5A0]' />
                                         <div>
                                             <h1 className='font-bold text-[20px] text-[#00E5A0] pt-5'>{f.name}</h1>
-                                            <p className='text-wrap pt-2'>{f.feedback}</p>
+                                            <p className='text-sm'>{f.email}</p>
+                                            <p className='text-wrap pt-2 text-[18px]'>{f.feedback}</p>
                                         </div>
                                     </div>
                                 </SwiperSlide>
@@ -121,9 +159,10 @@ const Feedback = () => {
                         <input type="text" className="input bg-white/4 border-0 border-b-2 border-[#00EA50] w-full" placeholder="Your Name" {...register("name", {required: true})} />
                         {errors.name?.type === "required" && <p className='text-red-500 p-2 '>Name field is required.</p>}
 
-                        <input type="file" className="file-input w-full border-0 border-b border-[#00EA50] mt-2" {...register("image", {required: true})}/>
+                        <input type="file" className="file-input w-full border-0 border-b border-[#00EA50] mt-2" {...register("image", {required: true, validate: files=> files[0]?.size <= 2 * 1024 * 1024 || "Image must be under 2MB"})}/>
                         <label className="label">Max size 2MB</label>
                         {errors.image?.type === "required" && <p className='text-red-500 p-2'>You must provide an image</p>}
+                        {errors.image?.type === 'validate' && <p className='text-red-500 p-2'>Image must be under 2MB</p>}
 
                         <label className="label md:text-xl text-[#00EA50]">Email</label>
                         <input type="email" className="input bg-white/4 border-0 border-b-2 border-[#00EA50] w-full" placeholder="Email" {...register("email", {required: true})} />
@@ -132,8 +171,11 @@ const Feedback = () => {
                         <label className='label md:text-xl text-[#00EA50]'>Feedback</label>
                         <textarea placeholder="Your feedback" className="textarea textarea-accent bg-white/4 border-0 border-b-2 border-[#00EA50] w-full" {...register("feedback", {required: true, maxLength: 130})}></textarea>
                         {errors.feedback?.type === "required" && <p className='font-bold text-red-500 uppercase'>You forgot the most important thing...!!</p>}
+                        {errors.feedback?.type === "maxLength" && <p className='font-bold text-red-500'>Please keep it under 130 character</p>}
 
-                        <button className="btn btn-neutral mt-4 shadow-[inset_0_0_40px_rgba(0,229,160,0.15)] 
+                        <button type='submit'
+                                disabled={submitting}
+                                className="btn btn-neutral mt-4 shadow-[inset_0_0_40px_rgba(0,229,160,0.15)] 
                                 cursor-pointer 
                                 bg-white/5 
                                 uppercase 
@@ -143,7 +185,7 @@ const Feedback = () => {
                                 hover:text-white
                                 transition-all
                                 duration-400
-                                linear">Submit</button>
+                                linear">{submitting ? 'Submitting......' : 'Submit'}</button>
                     </fieldset>
                 </form>
                 <button
