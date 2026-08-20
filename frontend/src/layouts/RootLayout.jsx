72963f913842d1316b38b46d069d1228c1d2c7e7
fahim-shareear <fahimshareear@gmail.com>
2026-08-20 13:18:43 +0600

    undoing everyhting related to certification context and page and provider

--- frontend/src/layouts/RootLayout.jsx
+++ frontend/src/layouts/RootLayout.jsx
@@ -1,17 +1,14 @@
 import { Outlet } from "react-router";
 import Navbar from "../pages/Navbar";
 import Footer from "../pages/Footer";
-import { CertificationProvider } from "../authcontext/context/CertificationContext";
 
 const RootLayout = () => {
     return (
         <>
             <div className="relative">
-                <CertificationProvider>
                     <Navbar></Navbar>
                     <Outlet></Outlet>
                     <Footer></Footer>
-                </CertificationProvider>
             </div>
         </>
     )
