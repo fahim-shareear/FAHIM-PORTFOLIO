c2aa9708ab54ae7bd61926c1636e8dd811173b6d
fahim-shareear <fahimshareear@gmail.com>
2026-08-27 14:27:18 +0600

    correctly designing the log in button

--- frontend/src/pages/logbutton/Logbutton.jsx
+++ frontend/src/pages/logbutton/Logbutton.jsx
@@ -1,9 +1,31 @@
-
+import { useState } from "react";
+import { Link } from "react-router";
+import "../../all-css/logbutton.css";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { IoCloseCircle } from "react-icons/io5";
 
 const Logbutton = () => {
+    const [logOpen, setLogOpen] = useState(false);
+
+    const handleLogButton = () => {
+        setLogOpen(!logOpen);
+    };
+
     return (
-        <div>
-            
+        <div className={`log-panel ${logOpen ? "open" : ""}`}>
+            <div className="log-icon" onClick={handleLogButton}>
+                {logOpen ? <IoCloseCircle /> : <GiHamburgerMenu />}
+            </div>
+
+            <div className="log-links">
+                <Link
+                    to="/login"
+                    className="log-link"
+                    onClick={() => setLogOpen(false)}
+                >
+                    Log In
+                </Link>
+            </div>
         </div>
     );
 };
