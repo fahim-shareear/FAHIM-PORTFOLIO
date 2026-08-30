122f36d128d881dbaf769edf47d385a56a9cfd23
fahim-shareear <fahimshareear@gmail.com>
2026-08-30 21:17:24 +0600

    setting the font size padding and color to the log in page error texts

--- frontend/src/pages/authentication/Login.jsx
+++ frontend/src/pages/authentication/Login.jsx
@@ -62,7 +62,7 @@ const Login = () => {
                         autoComplete="email"
                         {...register("email", { required: true })}
                     />
-                    {errors.email && <p className="auth-error">email is required</p>}
+                    {errors.email && <p className="auth-error p-2 capitalize text-sm text-red-500">email is required</p>}
 
                     <div className="relative">
                         <label className="field-label" htmlFor="password">
@@ -79,14 +79,14 @@ const Login = () => {
                         <button className="top-9 right-5 absolute text-xl text-[#00EA50] cursor-pointer" onClick={handleEye}>
                             {eye ? <FaEyeSlash /> : <FaEye />}
                         </button>
-                        {errors.password && <p className="auth-error">password is required</p>}
+                        {errors.password && <p className="auth-error p-2 capitalize text-sm text-red-500">password is required</p>}
                         { }
                     </div>
 
                     <div className="auth-form__meta">
                         <a className="auth-link">forgot password?</a>
                     </div>
-                    {errors.root && <p className="auth-error">{errors.root.message}</p>}
+                    {errors.root && <p className="auth-error text-sm uppercase text-red-500">{errors.root.message}</p>}
                     <button type="submit" className="auth-btn">
                         log in
                     </button>
