d08b5bc137fca64b99a8f4f18a391dd1ce5270fa
fahim-shareear <fahimshareear@gmail.com>
2026-09-07 23:32:19 +0600

    adding the certification post page design

--- frontend/src/pages/components/Dashboard/CertificationPost.jsx
+++ frontend/src/pages/components/Dashboard/CertificationPost.jsx
@@ -1,9 +1,28 @@
+import { useForm } from "react-hook-form";
 
 
 const CertificationPost = () => {
+    const { register, handleSubmit, reset } = useForm();
+
+
+
     return (
         <div>
-            <h1>Certifications post</h1>
+            <div>
+                <h1>Certification and Career timeline post dashboard:</h1>
+            </div>
+            <div>
+                <form>
+                    <fieldset className="fieldset">
+                        <label className="label">Email</label>
+                        <input type="email" className="input" placeholder="Email" />
+                        <label className="label">Password</label>
+                        <input type="password" className="input" placeholder="Password" />
+                        <div><a className="link link-hover">Forgot password?</a></div>
+                        <button className="btn btn-neutral mt-4">Login</button>
+                    </fieldset>
+                </form>
+            </div>
         </div>
     );
 };
